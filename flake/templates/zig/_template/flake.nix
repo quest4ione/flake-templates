50c7c75c7175a34c547c68ecd515a8c8add9b0a8
quest4ione <quest4ione@gmail.com>
2026-08-25 21:05:15 +0200

    feat: use import-tree in templates

--- flake/templates/zig/_template/flake.nix
+++ flake/templates/zig/_template/flake.nix
@@ -1,14 +1,11 @@
 {
-inputs = {
+  inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+    import-tree.url = "github:denful/import-tree";
   };
 
-  outputs = inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } (_: {
-      imports = inputs.nixpkgs.lib.filesystem.listFilesRecursive ./flake;
-    });
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; }
+    (inputs.import-tree ./flake);
 }
