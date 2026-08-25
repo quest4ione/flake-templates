3c300ea0deac0d14d175fce3e50ffc222f08a83d
quest4ione <quest4ione@gmail.com>
2026-08-25 21:59:14 +0200

    chore: add nixpkgs input

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
     import-tree.url = "github:denful/import-tree";
   };
 
