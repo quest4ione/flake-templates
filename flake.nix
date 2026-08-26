126455e1df576e7b5cadb5c4d22190cc60675b2f
quest4ione <quest4ione@gmail.com>
2026-08-26 21:24:02 +0200

    chore: add nix-wrapper-modules input

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
     import-tree.url = "github:denful/import-tree";
+    nix-wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
+    nix-wrapper-modules.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./flake);
